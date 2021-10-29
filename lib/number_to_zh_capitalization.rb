d33da5195342977dd2a31bfedd3146a35ee9a02e
Lion <lion@agideo.com>
2021-10-29 13:52:30 +0800

    缩进

--- lib/number_to_zh_capitalization.rb
+++ lib/number_to_zh_capitalization.rb
@@ -20,54 +20,54 @@ class NumberToZhCapitalization
 
   private
 
-    def split_int_and_cent(num)
-      int = num.to_i
-      values = [int.to_s]
+  def split_int_and_cent(num)
+    int = num.to_i
+    values = [int.to_s]
 
-      values << (carry_num(num) - carry_num(int)).to_s.rjust(ZH_CENTS.length, '0') if int != num
-      values
+    values << (carry_num(num) - carry_num(int)).to_s.rjust(ZH_CENTS.length, '0') if int != num
+    values
+  end
+
+  def carry_num(num)
+    (num * (10**ZH_CENTS.length)).to_i
+  end
+
+  def get_zh_num(char)
+    ZH_NUMS[char.to_i]
+  end
+
+  def minus_word
+    @number < 0 ? '负' : ''
+  end
+
+  def int_words(int, length)
+    return '' if int == '0'
+    raise 'your number length is too more' if int.length > (ZH_SPLIT_DELIMITER.length * length)
+
+    slice_arr = int.split('').reverse.each_slice(length).map do |i|
+      i.reverse.join.rjust(length, '0').sub(/0+$/, '')
     end
 
-    def carry_num(num)
-      (num * (10**ZH_CENTS.length)).to_i
-    end
-
-    def get_zh_num(char)
-      ZH_NUMS[char.to_i]
-    end
-
-    def minus_word
-      @number < 0 ? '负' : ''
-    end
-
-    def int_words(int, length)
-      return '' if int == '0'
-      raise 'your number length is too more' if int.length > (ZH_SPLIT_DELIMITER.length * length)
-
-      slice_arr = int.split('').reverse.each_slice(length).map do |i|
-        i.reverse.join.rjust(length, '0').sub(/0+$/, '')
+    slice_arr.map.with_index do |num_str, idx|
+      words = ""
+      words << get_num_delimiter_words(num_str) if num_str.length > 0
+      if num_str.length > 0 || (num_str.length == 0 && idx.even? && slice_arr[idx + 1].to_s.length > 0)
+        words << ZH_SPLIT_DELIMITER[idx]
       end
+      words
+    end.compact.reverse.join.gsub(/零+/, '零').sub(/^零/, '') + '元'
+  end
 
-      slice_arr.map.with_index do |num_str, idx|
-        words = ""
-        words << get_num_delimiter_words(num_str) if num_str.length > 0
-        if num_str.length > 0 || (num_str.length == 0 && idx.even? && slice_arr[idx + 1].to_s.length > 0)
-          words << ZH_SPLIT_DELIMITER[idx]
-        end
-        words
-      end.compact.reverse.join.gsub(/零+/, '零').sub(/^零/, '') + '元'
-    end
+  def get_num_delimiter_words(str_num)
+    str_num.each_char.map.with_index do |char, idx|
+      [get_zh_num(char), char != '0' ? ZH_NUM_DELIMITER[idx] : ''].join
+    end.join
+  end
 
-    def get_num_delimiter_words(str_num)
-      str_num.each_char.map.with_index do |char, idx|
-        [get_zh_num(char), char != '0' ? ZH_NUM_DELIMITER[idx] : ''].join
-      end.join
-    end
-
-    def cent_words(str_num)
-      return '整' if str_num.to_i == 0
-      str_num.each_char.map.with_index do |char, idx|
-        [get_zh_num(char), ZH_CENTS[idx]].join if char != '0'
-      end.compact.join
-    end
+  def cent_words(str_num)
+    return '整' if str_num.to_i == 0
+    str_num.each_char.map.with_index do |char, idx|
+      [get_zh_num(char), ZH_CENTS[idx]].join if char != '0'
+    end.compact.join
+  end
 end
